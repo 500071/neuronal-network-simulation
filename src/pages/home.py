30e5d15c68320fd42100440a1bb3c8cdf06fb0b1
500071 <114418583+500071@users.noreply.github.com>
2024-05-05 20:56:06 +0200

    Update home.py

--- src/pages/home.py
+++ src/pages/home.py
@@ -13,6 +13,16 @@ def layout():
         html.Br(),
         html.H1("Neuronal network simulation"),
         "This website was created for the purposes of research activities of the ",
-        "Nonlinear Dynamics Team at the Department of Mathematics and Statistics, Faculty of Science, Masaryk University."
+        "Nonlinear Dynamics Team at the Department of Mathematics and Statistics, Faculty of Science, Masaryk ",
+        "University. This website was created as a part of the GAMU Interdisciplinary MUNI/G/1213/2022 project on ",
+        "Mathematical modeling of very and ultra-fast oscillations in EEG signals",
+        html.Br(),
+        "This website is a tool for modelling of complex networks of neurons. It supports three models of neurons — ",
+        "Morris-Lecar model, interneuron model and Destexhe-Paré model. We model coupling of neurons using random ",
+        "networks, small world networks, ring networks and all to all networks. We allow coupling of the neurons into "
+        "one or more coupled clusters.",
+        html.Br(),
+        "To see the definitions of the models, see the 'INFO' tab. To run the simulations, go to the 'MORRIS-LECAR', "
+        "'INTERNEURON' and 'DESTEXHE-PARÉ' tabs."
     ], style={'margin-left': '110px', 'padding-right': '110px'})
     return layout
