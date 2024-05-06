452b492de72bf089d71244ad541bbdeea213399e
500071 <114418583+500071@users.noreply.github.com>
2024-05-06 16:21:31 +0200

    Update DP.py added comments

--- src/pages/DP.py
+++ src/pages/DP.py
@@ -1,14 +1,17 @@
-from dash import html, register_page, dcc  #, callback # If you need callbacks, import it here.
+# PAGE FOR SINGLE CLUSTER DESTEXHE-PARÉ SIMULATION
+# AUTHOR: Markéta Trembaczová, 2024
+
+from dash import html, register_page, dcc
 from dash import Input, Output, callback, State
+from math import sqrt
 import scipy.integrate as integrate
+from scipy.stats import truncnorm
+from scipy.signal import periodogram, detrend
 import numpy as np
 import random
 import networkx as nx
-from scipy.stats import truncnorm
 import plotly.express as px
 import plotly.graph_objects as go
-from scipy import signal
-from math import sqrt
 
 register_page(
     __name__,
@@ -16,11 +19,13 @@ register_page(
     top_nav=True,
     path='/DP'
 )
-######################################################################################################
-# SITE PRO SPRAZENI
 
+
+######################################################################################################
+# NETWORKS FOR COUPLING
 
 def no_coupling(n):
+    """returns adjacency matrix of a network of n neurons with no coupling"""
     K = []
     for row in range(n):
         line = []
@@ -31,12 +36,14 @@ def no_coupling(n):
 
 
 def small_world(n, k, p, seed):
-    # n..pocet neuronu, k..s kolika neurony je propojen, p..pst prepojeni
+    """returns adjacency matrix of a small world network of n neurons, each node is joined with its k nearest neighbors
+     in a ring topology, p is the probability of rewiring each edge"""
     G = nx.watts_strogatz_graph(n, k, p, seed)
     return nx.adjacency_matrix(G).todense()
 
 
 def all_to_all(n):
+    """returns adjacency matrix of an all to all network of n neurons"""
     K = []
     for row in range(n):
         line = []
@@ -50,36 +57,42 @@ def all_to_all(n):
 
 
 def random_network(n, p, seed):
-    G = nx.erdos_renyi_graph(n, p, seed, directed=False)  # random network
+    """returns adjacency matrix of a random network of n neurons, p is the probability of edge creation"""
+    G = nx.erdos_renyi_graph(n, p, seed, directed=False)
     return nx.adjacency_matrix(G).todense()
 
 
 def ring(n):
+    """returns adjacency matrix of a ring network of n neurons"""
     if n == 1:
         return np.asarray([[0]])
     K = []
     for row in range(n):
         line = []
         for col in range(n):
-            if col == (row-1) % n or col == (row+1) % n:
+            if col == (row - 1) % n or col == (row + 1) % n:
                 line.append(1)
             else:
                 line.append(0)
         K.append(line)
     return np.asarray(K)
 
+
 ######################################################################################################
-# MODEL NEURONU
+# MODEL DEFINITION
 
 def dp(p, alpha, beta):
     return alpha*(1-p) - beta*p
 
 
 def destexhe_pare(t, variables, params, stimulus):
-    # variables = [Vi, mi, hi, ni, mMi]
-    # params = [C, Iext, gNa, gK, VNa, VK, VT, VS, C[neuron]
-    # stimulus = [st_len, st_t0, st_A, st_r]
+    """function for definition of the single destexhe-paré neurons, returns increments of the variables
+        # variables = [Vi, mi, hi, ni, mMi]
+        # params = [C, Iext, gNa, gK, VNa, VK, VT, VS, C[neuron]
+        # stimulus = [st_t0[neuron], st_tn[neuron], st_A[neuron], st_r[neuron]]
+        """
 
+    # variables
     V = variables[0]
     m = variables[1]
     h = variables[2]
@@ -91,7 +104,7 @@ def destexhe_pare(t, variables, params, stimulus):
     Iext = params[1]
     A = stimulus[2]
     r = stimulus[3]
-    if stimulus[1] <= t <= stimulus[1] + stimulus[0]:
+    if stimulus[1] <= t <= stimulus[1] + stimulus[0]:  # adds stimulus on given interval
         Iext += A * np.exp(-r * (t - stimulus[1]))
     g_Na = params[2]
     g_Kdr = params[3]
@@ -120,7 +133,7 @@ def destexhe_pare(t, variables, params, stimulus):
     beta_mM = (-0.0001 * (V + 30)) / (1 - np.exp(V + 30) / 9)
     dmM = dp(mM, alpha_mM, beta_mM)
 
-    # function
+    # model definition
     I_L = g_L * (V - V_L)
     I_Na = g_Na * m ** 3 * h * (V - V_Na)
     I_Kdr = g_Kdr * n ** 4 * (V - V_K)
@@ -132,13 +145,13 @@ def destexhe_pare(t, variables, params, stimulus):
 
 
 def coupled_DP(t, variables, K, n, params, stimulus):
-    # prvne vsechny V, pak vsechny N
+    """function for adding coupling to the interneurons, returns increments of the variables
+        # variables = [Vi, mi, hi, ni, mMi]
+        # params = [C, Iext, gNa, gK, VNa, VK, VT, VS]
+        # stimulus = [st_t0, st_tn, st_A, st_r]
+        """
 
     Vi = variables[:n]
-    mi = variables[n:2*n]
-    hi = variables[2*n:3*n]
-    ni = variables[3*n:4*n]
-    mMi = variables[4*n:]
 
     dV = []
     dm = []
@@ -146,11 +159,12 @@ def coupled_DP(t, variables, K, n, params, stimulus):
     dn = []
     dmM = []
 
+    # computes the increment for each single neuron
     for neuron in range(n):
         stimulus_neuron = [stimulus[0]]
         for index in range(1, 4):
-            stimulus_neuron.append(stimulus[index][neuron])
-            params[8] = params[0][neuron]
+            stimulus_neuron.append(stimulus[index][neuron])  # correct values of stimulus for each neuron
+            params[8] = params[0][neuron]  # correct value of C for each neuron
         dVar = destexhe_pare(t, [variables[neuron], variables[neuron+n], variables[neuron+2*n],  variables[neuron+3*n],
                                  variables[neuron+4*n]], params, stimulus_neuron)
         dV.append(dVar[0])
@@ -159,6 +173,7 @@ def coupled_DP(t, variables, K, n, params, stimulus):
         dn.append(dVar[3])
         dmM.append(dVar[4])
 
+    # adds coupling
     for row in range(n):
         for col in range(n):
             dV[row] += (Vi[col] - Vi[row]) * K[row][col] / params[0][row]
@@ -169,43 +184,47 @@ def coupled_DP(t, variables, K, n, params, stimulus):
     dV.extend(dmM)
     return dV
 
+
 ######################################################################################################
 # EULER-MARUYAMA METODA
 
-def euler_maruyama(sigma_noise, X0, T, dt, N_eq, K, params, stimulus):
-    # dX =  f(X) dt + sigma dW
-    # f...drift function (fce interneuron s parametry)
-    # sigma_noise = sigma_noice / C
-    # X0... pocatecni podminky
-    # T... delka casoveho intervalu
-    # dt... krok
-    # N_eq... pocet neuronu
+def euler_maruyama(sigma_noise, X0, T, dt, n_neurons, K, params, stimulus):
+    """function for the Euler-Maruyama numeric method, returns solution and the time vector.
+        dX =  f(X) dt + sigma dW
+        sigma_noise is the standard deviation of the noised input, X0 are the initial conditions, T is the length of
+        the time interval and dt is the time step, n_neurons is the number of neurons and K is the adjacency matrix of
+        the network.
+         params = [C, Iext, gNa, gK, VNa, VK, VT, VS]
+        stimulus = [st_t0, st_tn, st_A, st_r]
+    """
 
-    N = np.floor(T/dt).astype(int) # pocet kroku
-    d = len(X0) # pocet rovnic v soustave celkem
+    N = np.floor(T / dt).astype(int)  # number of steps
+    d = len(X0)  # number of differential equations
     sigma = []
-    for neuron in range(N_eq):
-        sigma.append(sigma_noise / sqrt(params[0][neuron]))
-    sigma_noise = sigma + [0] * (d - N_eq)
-    X = np.zeros((d, N + 1)) # pocet radku: pocet rovnic interneuronu, pocet sloupcu: pocet kroku+1
-    X[:, 0] = X0 # prvni sloupec jsou pocatecni podminky
-    t = np.arange(0, T+dt, dt)  #cas
-    dW = np.vstack([np.sqrt(dt) * np.random.randn(N_eq, N), np.zeros((d - N_eq, N))]) # prvni radek prirustky Wienerova procesu, pak nuly
+    for neuron in range(n_neurons):
+        sigma.append(sigma_noise / sqrt(params[0][neuron]))  # sigma_noise/sqrt(C_i)
+    sigma_noise = sigma + [0] * (d - n_neurons)
+    X = np.zeros((d, N + 1))
+    X[:, 0] = X0
+    t = np.arange(0, T + dt, dt)
+    dW = np.vstack([np.sqrt(dt) * np.random.randn(n_neurons, N),  # increments of Wiener's process
+                    np.zeros((d - n_neurons, N))])
 
-    for step in range(N): #pres vsechny kroky
-        neuron = coupled_DP(t[step], X[:, step], K, N_eq, params, stimulus)
+    for step in range(N):
+        neuron = coupled_DP(t[step], X[:, step], K, n_neurons, params, stimulus)  # solution without noise
         for i in range(len(neuron)):
-            neuron[i] = neuron[i] * dt
-        X[:, step + 1] += X[:, step] + neuron + sigma_noise * dW[:, step]
+            neuron[i] = neuron[i] * dt  # multiply the solution without noise by the time step
+        X[:, step + 1] += X[:, step] + neuron + sigma_noise * dW[:, step]  # EM method
     return X, t
+
 
 ######################################################################################################
 # TRUNCATED NORMAL DISTRIBUTION
 
-
 def random_truncnorm(a, b, mu, sigma, n):
+    """returns n numbers from truncated normal distribution TN(mu, sigma, a, b)"""
     if sigma == 0:
-        if n==1:
+        if n == 1:
             return [mu]
         else:
             return [mu] * n
@@ -295,7 +314,8 @@ def layout():
                 " σ = ", dcc.Input(id='C_sigma', value=0.001, type='number', size='5'),
                 " [ ", dcc.Input(id='C_a', value=0.95, type='number', size='5'),
                 ", ", dcc.Input(id='C_b', value=1.05, type='number', size='5'), " ])",
-            ], title="membrane capacitance, random from truncated normal distribution with given parameters for each neuron"),
+            ], title="membrane capacitance, random from truncated normal distribution with given parameters for each "
+                     "neuron"),
         ], style=dict(display='flex')),
         html.Br(),
         html.Div([
@@ -317,7 +337,8 @@ def layout():
                 " σ = ", dcc.Input(id='st_t0_sigma', value=0, type='number', size='5'),
                 " [ ", dcc.Input(id='st_t0_a', value=0, type='number', size='5'),
                 ", ", dcc.Input(id='st_t0_b', value=0, type='number', size='5'), " ])",
-            ], title="start of the stimulus, random from truncated normal distribution with given parameters for each neuron"),
+            ], title="start of the stimulus, random from truncated normal distribution with given parameters for each "
+                     "neuron"),
             html.Span(' ', style={'display': 'inline-block', 'width': '10px'}),
             html.Div([
                 "T_st = ", dcc.Input(id='st_tn', value=20, type='number', size='5')
@@ -332,7 +353,8 @@ def layout():
                 " σ = ", dcc.Input(id='st_A_sigma', value=0, type='number', size='5'),
                 " [ ", dcc.Input(id='st_A_a', value=0, type='number', size='5'),
                 ", ", dcc.Input(id='st_A_b', value=0, type='number', size='5'), " ])",
-            ], title="amplitude of the stimulus, random from truncated normal distribution with given parameters for each neuron"),
+            ], title="amplitude of the stimulus, random from truncated normal distribution with given parameters for "
+                     "each neuron"),
             html.Span(' ', style={'display': 'inline-block', 'width': '10px'}),
             html.Div([
                 "r ∼ TN(",
@@ -340,7 +362,8 @@ def layout():
                 " σ = ", dcc.Input(id='st_r_sigma', value=0, type='number', size='5'),
                 " [ ", dcc.Input(id='st_r_a', value=0, type='number', size='5'),
                 ", ", dcc.Input(id='st_r_b', value=0, type='number', size='5'), " ])",
-            ], title=" damping rate of the stimulus, random from truncated normal distribution with given parameters for each neuron"),
+            ], title=" damping rate of the stimulus, random from truncated normal distribution with given parameters "
+                     "for each neuron"),
         ], style=dict(display='flex')),
         html.Br(),
 
@@ -360,21 +383,23 @@ def layout():
 
 ######################################################################################################
 # CALLBACK
+# function for what happens after clicking on the button
 
 
 @callback(
     Output(component_id='plots_DP', component_property='children'),
     Input('button_DP', 'n_clicks'),
     [State('n', 'value'), State('eps', 'value'), State('coupling-type', 'value'), State('p', 'value'),
-     State('k', 'value'), State('seed', 'value'), State('V0beg', 'value'),State('V0end', 'value'), State('m0', 'value'),
-     State('h0', 'value'), State('n0', 'value'), State('nM0', 'value'), State('t0', 'value'), State('tn', 'value'),
-     State('dt', 'value'), State('Iext', 'value'), State('st_t0', 'value'), State('st_t0_sigma', 'value'),
-     State('st_t0_a', 'value'), State('st_t0_b', 'value'), State('st_tn', 'value'), State('st_A', 'value'),
-     State('st_A_sigma', 'value'), State('st_A_a', 'value'), State('st_A_b', 'value'), State('st_r', 'value'),
-     State('st_r_sigma', 'value'), State('st_r_a', 'value'), State('st_r_b', 'value'), State('C_mu', 'value'),
-     State('C_sigma', 'value'), State('C_a', 'value'), State('C_b', 'value'), State('gNa', 'value'),
-     State('gK', 'value'), State('VNa', 'value'), State('VK', 'value'), State('VT', 'value'), State('VS', 'value'),
-     State('sigma_noise', 'value'), State('periodogram_a', 'value'), State('periodogram_b', 'value')
+     State('k', 'value'), State('seed', 'value'), State('V0beg', 'value'), State('V0end', 'value'),
+     State('m0', 'value'), State('h0', 'value'), State('n0', 'value'), State('nM0', 'value'), State('t0', 'value'),
+     State('tn', 'value'), State('dt', 'value'), State('Iext', 'value'), State('st_t0', 'value'),
+     State('st_t0_sigma', 'value'), State('st_t0_a', 'value'), State('st_t0_b', 'value'), State('st_tn', 'value'),
+     State('st_A', 'value'), State('st_A_sigma', 'value'), State('st_A_a', 'value'), State('st_A_b', 'value'),
+     State('st_r', 'value'), State('st_r_sigma', 'value'), State('st_r_a', 'value'), State('st_r_b', 'value'),
+     State('C_mu', 'value'), State('C_sigma', 'value'), State('C_a', 'value'), State('C_b', 'value'),
+     State('gNa', 'value'), State('gK', 'value'), State('VNa', 'value'), State('VK', 'value'), State('VT', 'value'),
+     State('VS', 'value'), State('sigma_noise', 'value'), State('periodogram_a', 'value'),
+     State('periodogram_b', 'value')
      ]
 )
 def update_output_DP(n_clicks, n, epsilon, coupling_type, p, k, seed, V0_beg, V0_end, m0_init, h0_init, n0_init,
@@ -382,7 +407,7 @@ def update_output_DP(n_clicks, n, epsilon, coupling_type, p, k, seed, V0_beg, V0
                      st_A_a, st_A_b, st_r_mu, st_r_sig, st_r_a, st_r_b, C_mu, C_sigma, C_a, C_b, gNa, gK, VNa, VK, VT,
                      VS, sigma_noise, periodogram_a, periodogram_b):
     if n_clicks > 0:
-        # GENEROVANI MATICE K
+        # GENERATE COUPLING MATRIX K
         if coupling_type == 'all to all':
             K = all_to_all(n)
         elif coupling_type == 'small world':
@@ -395,7 +420,7 @@ def update_output_DP(n_clicks, n, epsilon, coupling_type, p, k, seed, V0_beg, V0
             K = no_coupling(n)
         K = K * epsilon
 
-        # POCATECNI PODMINKY
+        # INITIAL CONDITIONS
         V0 = []
         m0 = []
         h0 = []
@@ -416,13 +441,12 @@ def update_output_DP(n_clicks, n, epsilon, coupling_type, p, k, seed, V0_beg, V0
         stimulus = [st_len, st_t0, st_A, st_r]
         print(st_r)
 
-
-        # VOLBA PARAMETRU
+        # PARAMETERS
         C = random_truncnorm(C_a, C_b, C_mu, C_sigma, n)
         params = [C, Iext, gNa, gK, VNa, VK, VT, VS, C[0]]
 
-        # RESENI
-        if sigma_noise == 0:
+        # SOLUTION
+        if sigma_noise == 0:  # standard deviation of the added noise
             # using Runge Kutta 45
             res = integrate.solve_ivp(coupled_DP, [t0, tn], y0, method='RK45', args=[K, n, params, stimulus])
             V = res.y
@@ -431,28 +455,21 @@ def update_output_DP(n_clicks, n, epsilon, coupling_type, p, k, seed, V0_beg, V0
             # using Euler Maruyama
             V, T = euler_maruyama(sigma_noise, y0, tn, dt, n, K, params, stimulus)
 
-        # VYPOCTY PRO VIZUALIZACI
+        # COMPUTATIONS FOR VISUALIZATIONS
         Vsum = []
         for i in range(len(V[0])):
             Vsum.append(0)
             for neuron in range(n):
                 Vsum[i] += V[neuron][i]
 
-        ymin = min(Vsum)
-        ymax = max(Vsum)
-        for neuron in range(n):
-            ymin = min(ymin, min(V[neuron]))
-            ymax = max(ymax, max(V[neuron]))
-
-        # NEURONY SAMOSTATNE
+        # SINGLE NEURONS
         fig_single = px.line(title='Single coupled neurons')
         for neuron in range(n):
-            # plt.plot(T, V[neuron], 'blue')
             fig_single.add_trace(px.line(x=T, y=V[neuron]).data[0])
         fig_single.update_xaxes(title_text='t [ms]')
         fig_single.update_yaxes(title_text='V_i [mV]')
 
-        # SOUCET NEURONU
+        # SUM OF THE NEURONS
         fig_sum = px.line(y=Vsum, x=T, title='Sum of coupled the neurons')
         fig_sum.update_xaxes(title_text='t [ms]')
         fig_sum.update_yaxes(title_text='V [mV]')
@@ -463,21 +480,21 @@ def update_output_DP(n_clicks, n, epsilon, coupling_type, p, k, seed, V0_beg, V0
             if periodogram_a <= T[index] <= periodogram_b:
                 V_periodogram.append(Vsum[index])
         fs = 1000 * len(V_periodogram) / tn
-        f, Pxx = signal.periodogram(signal.detrend(V_periodogram), fs)
+        f, Pxx = periodogram(detrend(V_periodogram), fs)
         kernel = np.array([1, 1, 1]) / 3
         smoothed_Pxx = np.convolve(Pxx, kernel, mode='same')
         f_smoothed = f[:len(smoothed_Pxx)]
         limit = 2000
-        treshold = 0.1
+        threshold = 0.1
         for i in range(round(limit / 2), len(f_smoothed)):
-            if smoothed_Pxx[i] > treshold:
+            if smoothed_Pxx[i] > threshold:
                 limit = i * 5 + 100
         fig_periodogram = px.line(y=smoothed_Pxx, x=f_smoothed, title='Periodogram')
         fig_periodogram.update_layout(xaxis_range=[0, limit])
         fig_periodogram.update_xaxes(title_text='frequency [Hz]')
         fig_periodogram.update_yaxes(title_text='|P(f)|')
 
-        # VIZUALIZACE SITE
+        # NETWORK VISUALIZATION
         fig_K = go.Figure(data=go.Heatmap(
             z=K,
             colorscale="purples",
